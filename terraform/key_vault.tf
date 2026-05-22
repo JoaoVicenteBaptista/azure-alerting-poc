7709fc952053b8563f658930f843c30cc56392c1
Joao Baptista <joaobaptista@hotmail.com>
2026-05-22 18:18:21 +0100

    feat: add key vault and resource group

---
+++ terraform/key_vault.tf
@@ -0,0 +1,55 @@
+# Random suffix for global uniqueness
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.project_name}-${var.environment}"
+  location = var.location
+
+  tags = {
+    environment = var.environment
+    project     = var.project_name
+    cost-center = "poc"
+  }
+}
+
+resource "azurerm_key_vault" "main" {
+  name                       = "kv-${var.project_name}-${random_string.suffix.result}"
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  sku_name                   = "standard"
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+
+  tags = {
+    environment = var.environment
+    project     = var.project_name
+    cost-center = "poc"
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "terraform" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+  ]
+}
+
+resource "azurerm_key_vault_secret" "teams_webhook" {
+  name         = "teams-webhook-url"
+  key_vault_id = azurerm_key_vault.main.id
+  value        = var.teams_webhook_url
+
+  depends_on = [azurerm_key_vault_access_policy.terraform]
+}
